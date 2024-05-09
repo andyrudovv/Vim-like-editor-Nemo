e5893ab7b096c2e3521fc88e740f6b59cd3fe380
andyrudovv <110022906+andyrudovv@users.noreply.github.com>
2024-05-09 21:49:59 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use std::io::stdout;
 
-fn main() {
-    println!("Hello, world!");
+use crossterm::{event::read, terminal, ExecutableCommand};
+
+
+fn main() -> anyhow::Result<()> {
+    let mut stdout = stdout();
+
+    terminal::enable_raw_mode()?;
+    stdout.execute(terminal::EnterAlternateScreen)?;
+
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+    read()?;
+    read()?;
+
+    stdout.execute(terminal::LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
 }
+
